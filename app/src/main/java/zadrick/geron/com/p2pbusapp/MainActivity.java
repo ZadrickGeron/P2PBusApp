512a8b10b0bf2b2b17a02bc544da9336c964e0c1
ZadrickGeron <2014073345@ust-ics.mygbiz.com>
2018-11-17 22:28:47 +0800

    Week 2

--- app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
+++ app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
@@ -1,7 +1,9 @@
 package zadrick.geron.com.p2pbusapp;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+    public void locate(View V){
+        MyLocation.LocationResult locationResult;
+        locationResult = new MyLocation.LocationResult(){
+            @Override
+            public void gotLocation(Location location){
+                //Got the location!
+            }
+        };
+        MyLocation myLocation = new MyLocation();
+        myLocation.getLocation(this, locationResult);
     }
 }
