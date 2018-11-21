b77fd6bba91a70af22b679580f449af5d64d3c44
ZadrickGeron <2014073345@ust-ics.mygbiz.com>
2018-11-21 19:38:36 +0800

    Week 3

--- app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
+++ app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
@@ -1,15 +1,32 @@
 package zadrick.geron.com.p2pbusapp;
 
+import android.content.Intent;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        loginbutton = (Button) findViewById(R.id.loginbutton);
+        loginbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openallstat();
+            }
+        });
+    }
+
+    public void openallstat() {
+        Intent intent = new Intent(this, allstat.class);
+        startActivity(intent);
     }
 }
