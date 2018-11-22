01e955790520f267ecc5e6023b136c891047a539
ZadrickGeron <2014073345@ust-ics.mygbiz.com>
2018-11-23 04:18:58 +0800

    Week 3

--- app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
+++ app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
@@ -9,15 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginbutton;
+    private Button guestlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        loginbutton = (Button) findViewById(R.id.loginbutton);
-        loginbutton.setOnClickListener(new View.OnClickListener(){
+        guestlogin = (Button) findViewById(R.id.guestlogin);
+        guestlogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 openallstat();
