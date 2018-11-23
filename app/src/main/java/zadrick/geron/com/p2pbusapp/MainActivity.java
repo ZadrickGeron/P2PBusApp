06a181f6949d1b85b4f42208672ca8de95e9736d
ZadrickGeron <2014073345@ust-ics.mygbiz.com>
2018-11-23 22:59:46 +0800

    Week 3

--- app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
+++ app/src/main/java/zadrick/geron/com/p2pbusapp/MainActivity.java
@@ -1,21 +1,27 @@
 package zadrick.geron.com.p2pbusapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button guestlogin;
+    private Button login;
+    private EditText editText;
+    private EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
         guestlogin = (Button) findViewById(R.id.guestlogin);
         guestlogin.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -23,10 +29,28 @@ public class MainActivity extends AppCompatActivity {
                 openallstat();
             }
         });
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                if(editText.getText().toString().equals("admin") &&
+                        editText2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     public void openallstat() {
         Intent intent = new Intent(this, allstat.class);
         startActivity(intent);
     }
+
 }
